e4073023bf9a31d28dc41edd6d772f027d35e14d
Noa <noabendet@post.runi.ac.il>
2024-12-18 15:21:27 +0200

    Add: ask_user_input

--- video_search_by_video.py
+++ video_search_by_video.py
@@ -1,2 +1,19 @@
 def search_by_video():
     pass
+
+def connect_to_gemini():
+    pass
+
+def generate_prompt_by_user_input(user_input):
+    pass
+
+def ask_user_input():
+    print("Using a video model. What would you like me to find in the video?")
+    user_input = input()
+    while not user_input.strip():
+        print("Please type a word.")
+        user_input = input()
+    return user_input
+
+def chat_with_gemini(prompt):
+    pass
